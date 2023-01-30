77605dcab723e28c2c17e5581d26a63dabab3ed0
Umar <work.umar7@hotmail.com>
2023-01-30 01:00:01 +0000

    completed the GUI version of the game without comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # rock-paper-scissors
+A medieval twist to the famous rock, paper, scissors game. Have an attepmt at being the infamous knight.
+
+# Attributions 
+1.) Enemy rock image - <a href="https://www.freepik.com/free-vector/fantasy-alien-planets-ui-space-game-vector_28641103.htm#query=purple%20rock&position=40&from_view=search&track=sph">Image by upklyak</a> on Freepik
+2.) Shield images - <a href="https://www.freepik.com/free-vector/set-game-shields-cartoon-fantasy-medieval-armor-metal-wood-knight-ammo-iron-wooden-guard-collection-ui-design-elements-military-screens-front-view-isolated-vector-icons-clipart_21002624.htm#query=cartoon%20shield&position=0&from_view=search&track=sph">Image by upklyak</a> on Freepik
+3.) Main background - <a href="https://www.freepik.com/free-vector/abandoned-medieval-castle-with-gold-royal-thrones_9396120.htm#query=medieval%20cartoon%20background&position=14&from_view=search&track=ais">Image by upklyak</a> on Freepik
+4.) Background when you win - <a href="https://www.freepik.com/free-vector/knight-standing-with-sword-medieval-castle_21439213.htm#from_view=detail_alsolike">Image by upklyak</a> on Freepik
+5.) Background when you lose - <a href="https://www.freepik.com/free-vector/abandoned-castle-throne-room-with-dragon-fly-outside-window_13196120.htm#from_view=detail_alsolike">Image by upklyak</a> on Freepik
+6.) Sword images - <a href="https://www.freepik.com/free-vector/set-magic-swords-space-laser-futuristic-blades_21002617.htm#query=cartoon%20swords&position=2&from_view=search&track=sph">Image by upklyak</a> on Freepik
--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 'use strict';
 
+// Variables
+let computerLives = 5;
+let userLives = 5;
+let round = 0;
+
+// Selectors
+const loreAndRulesButtons = document.querySelectorAll('.lore-rules-buttons');
+const userRockButton = document.getElementById('rock-hero');
+const userPaperButton = document.querySelector('.button-shield-hero');
+const userScissorButton = document.querySelector('.button-sword-hero');
+const computerRockButton = document.querySelector('.button-rock-enemy');
+const computerPaperButton = document.querySelector('.button-shield-enemy');
+const computerScissorButton = document.querySelector('.button-sword-enemy');
+const userLivesText = document.querySelector('.user-lives');
+const computerLivesText = document.querySelector('.computer-lives');
+const roundText = document.querySelector('.round');
+const roundOutcome = document.querySelector('.round-text');
+const playAgainButton = document.querySelector('.play-again-button-container')
+const mainTitle = document.querySelector('.title');
+const infoButtons = document.querySelector('.info-buttons');
+const gameArea = document.querySelector('.game-section');
+
+for (let i = 0; i < loreAndRulesButtons.length; i++) {
+    if (i === 0) {
+        loreAndRulesButtons[i].addEventListener('click', function () {
+            document.querySelector('.modal-lore').classList.toggle('hidden');
+        });
+    } else if (i === 1) {
+        loreAndRulesButtons[i].addEventListener('click', function () {
+            document.querySelector('.modal-rules').classList.toggle('hidden');
+        });
+    }
+}
+
 // 1) Create a function that will generate the computer's choice
 const getComputerChoice = function () {
     let computerChoice = Math.trunc(Math.random() * 3) + 1; // Number generated between 1-3
@@ -15,76 +49,122 @@ const getComputerChoice = function () {
     }
 };
 
-//Function that takes user input
-const getPlayerChoice = function () {
-    let userChoice = prompt("choose rock, paper or scissor");
-    return userChoice;
-};
-
-//Function that verifies that the user's choice is either rock, paper or scissor, with the parameter being the user's choice
-const verifyPlayerChoice = function (choice) {
-    let validInput = false; //boolean value that is set to false and will turn to true if the user enters a valid value
-    if (typeof choice === "string") { //if the argument is of type string, we can turn it to lowercase, whereas if it's default we can't so that must be handled seperately
-        choice.toLowerCase();
-        if (choice !== "rock" && choice !== "paper" && choice !== "scissor") { //if the input isn't one of the expected values
-            while (validInput) {
-                choice = getPlayerChoice(); //we again ask for the user's choice
-                if (choice === "rock" || choice === "paper" || choice === "scissor") {
-                    validInput = true; //if the input is valid, then validInput is set to true and the while loop terminates
-                }
-            }
-        }
-    } else if (choice === null) { //in the event the user clicks cancel, the value returned would be null, and so we can quit the game entirely
-        alert("Game has been quit");
-        return;
+const chooseCorrectWeapon = function (weapon) {
+    if (computerRockButton.classList.contains('weapon-select-enemy')) computerRockButton.classList.toggle('weapon-select-enemy');
+    if (computerPaperButton.classList.contains('weapon-select-enemy')) computerPaperButton.classList.toggle('weapon-select-enemy');
+    if (computerScissorButton.classList.contains('weapon-select-enemy')) computerScissorButton.classList.toggle('weapon-select-enemy');
+    if (weapon === "rock") {
+        computerRockButton.classList.toggle('weapon-select-enemy');
+    } else if (weapon === "paper") {
+        computerPaperButton.classList.toggle('weapon-select-enemy');
+    } else if (weapon === "scissor") {
+        computerScissorButton.classList.toggle('weapon-select-enemy');
     }
-    return choice.toLowerCase(); // we can then return choice and make sure it's of lowercase
-};
+}
 
 //A function that will run a single round, with the parameters being the user's choice and the computer's choice
 const playRound = function (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissor"
         || playerSelection === "paper" && computerSelection === "rock"
         || playerSelection === "scissor" && computerSelection === "paper") {
-        return (`User won as they chose ${playerSelection} and computer chose ${computerSelection}`); //these are all the possible combinations of the user winning
+        computerLivesText.textContent = `Knight Lives Remaining: ${--computerLives}`;
+        roundText.textContent = `Round: ${++round}`;
+        if (playerSelection === "rock") {
+            roundOutcome.textContent = `The mighty hero pulls out his magic rock and shatters the knight's sword as he tries to attack`;
+        } else if (playerSelection === "paper") {
+            roundOutcome.textContent = `The hero rams their shield into the knight's powerful rock, causing a small exposion, stunning the knight`;
+        } else {
+            roundOutcome.textContent = `Out of nowehere the hero pulls their swords out and slices the knight's shield in half, and pushes a sword into the knight's armour`;
+        }
     } else if (playerSelection === computerSelection) {
-        return (`The game is a tie as you both have used ${playerSelection}`);
+        roundText.textContent = `Round: ${++round}`;
+        roundOutcome.textContent = `The town looks on as the two battle out fiercly, matching each move with a counter, neither of them get the upper-hand in this round`;
     } else {
-        return (`Computer won as the computer chose ${computerSelection} and the user chose ${playerSelection}`); //if it isn't a draw or if the user didn't win then the computer wins
+        userLivesText.textContent = `User Lives Remaining: ${--userLives}`;
+        roundText.textContent = `Round: ${++round}`;
+        if (computerSelection === "rock") {
+            roundOutcome.textContent = `The knight seems too powerful, pulling out his magic rock and blinding the town's hero, things are not looking good right now for the majestic hero`;
+        } else if (computerSelection === "paper") {
+            roundOutcome.textContent = `The knight stands strong, pulling his large purple shield out and prevents the damage from the hero's magic rock`;
+        } else {
+            roundOutcome.textContent = `The knight pulls out his large purple stone powered sword, and slashes the hero's shield. A menacing "muhahaha" can be heard coming from the knight's mouth`;
+        }
     }
 };
 
-//Function that executes the entire game, game would be best of 5
-const game = function () {
-    let userScore = 0;
-    let computerScore = 0;
-    let tie = 0;
-    let roundResult = ""; //this will be used to check whether the user or the computer won
-    for (let i = 0; i < 5; i++) { //for loop executes 5 times to make it a best of 5 game
-        let playerChoice = verifyPlayerChoice(getPlayerChoice()); //we get the player choice
-        if (playerChoice === undefined) { //if the player pressed cancel, the verifyPlayerChoice() function would return undefined, in that case we stop the game
-            break;
-        }
-        roundResult = playRound(playerChoice, getComputerChoice()); //play round returns a string based on who won
-        alert(roundResult); //we show that string to the user as an alert
+const scoreChecker = function (userScore, computerScore) {
+    if (userScore < 1) {
+        document.body.style.backgroundImage = "url('losing-screen.jpg')";
+        mainTitle.classList.toggle("hidden");
+        infoButtons.classList.toggle("hidden");
+        gameArea.classList.toggle("hidden");
+        document.querySelector('.user-side').style.display = "none";
+        document.querySelector('.computer-side').style.display = "none";
+        document.querySelector('.game-information').style.display = "none";
+        document.querySelector('.modal-loss').classList.toggle('hidden');
+    } else if (computerScore < 1) {
+        document.body.style.backgroundImage = "url('winning-screen.jpg')";
+        mainTitle.classList.toggle("hidden");
+        infoButtons.classList.toggle("hidden");
+        gameArea.classList.toggle("hidden");
+        document.querySelector('.user-side').style.display = "none";
+        document.querySelector('.computer-side').style.display = "none";
+        document.querySelector('.game-information').style.display = "none";
+        document.querySelector('.modal-win').classList.toggle('hidden');
+    }
+};
 
-        //this if...else statement compares whether the string starts with user or computer, and from there we know who won that round and we increment the appropriate score
-        if (roundResult.startsWith("User")) {
-            userScore++;
-        } else if (roundResult.startsWith("Computer")) {
-            computerScore++;
-        } else {
-            tie++;
-        }
-        alert(`The score is wins: ${userScore} - loss: ${computerScore} - ties: ${tie}`); //display the current score after each round
-    }
-    //Displays final result after all rounds have been completed
-    alert(`The  final score is wins: ${userScore} - loss: ${computerScore} - ties: ${tie}`);
-    if (userScore > computerScore) {
-        alert(`The user wins ${userScore} - ${computerScore}`);
-    } else if (userScore === computerScore) {
-        alert(`The game is tied ${computerScore} - ${userScore}`);
-    } else {
-        alert(`The computer wins ${computerScore} - ${userScore}`);
-    }
-}
+playAgainButton.addEventListener('click', function () {
+    computerLives = 5;
+    userLives = 5;
+    round = 0;
+    if (userRockButton.classList.contains('selected-weapon')) userRockButton.classList.toggle('selected-weapon');
+    if (userPaperButton.classList.contains('selected-weapon')) userPaperButton.classList.toggle('selected-weapon');
+    if (userScissorButton.classList.contains('selected-weapon')) userScissorButton.classList.toggle('selected-weapon');
+    if (computerRockButton.classList.contains('weapon-select-enemy')) computerRockButton.classList.toggle('weapon-select-enemy');
+    if (computerPaperButton.classList.contains('weapon-select-enemy')) computerPaperButton.classList.toggle('weapon-select-enemy');
+    if (computerScissorButton.classList.contains('weapon-select-enemy')) computerScissorButton.classList.toggle('weapon-select-enemy');
+    roundOutcome.textContent = '';
+    computerLivesText.textContent = `Knight Lives Remaining: ${computerLives}`
+    roundText.textContent = `Round: ${round}`;
+    userLivesText.textContent = `User Lives Remaining: ${userLives}`;
+    document.body.style.backgroundImage = "url('background-image.jpg')";
+    if (mainTitle.classList.contains('hidden')) mainTitle.classList.toggle('hidden');
+    if (infoButtons.classList.contains('hidden')) infoButtons.classList.toggle('hidden');
+    if (gameArea.classList.contains('hidden')) gameArea.classList.toggle('hidden');
+    if (document.querySelector('.game-information').style.display === "none") document.querySelector('.game-information').style.display = null;
+    if (document.querySelector('.user-side').style.display === "none") document.querySelector('.user-side').style.display = null;
+    if (document.querySelector('.computer-side').style.display === "none") document.querySelector('.computer-side').style.display = null;
+    if (!document.querySelector('.modal-loss').classList.contains('hidden')) document.querySelector('.modal-loss').classList.toggle('hidden');
+    if (!document.querySelector('.modal-win').classList.contains('hidden')) document.querySelector('.modal-win').classList.toggle('hidden');
+});
+
+userRockButton.addEventListener('click', () => {
+    userRockButton.classList.toggle('selected-weapon');
+    if (userPaperButton.classList.contains('selected-weapon')) userPaperButton.classList.toggle('selected-weapon');
+    if (userScissorButton.classList.contains('selected-weapon')) userScissorButton.classList.toggle('selected-weapon');
+    let computerChoice = getComputerChoice();
+    chooseCorrectWeapon(computerChoice);
+    playRound("rock", computerChoice);
+    scoreChecker(userLives, computerLives);
+});
+
+userPaperButton.addEventListener('click', () => {
+    userPaperButton.classList.toggle('selected-weapon');
+    if (userRockButton.classList.contains('selected-weapon')) userRockButton.classList.toggle('selected-weapon');
+    if (userScissorButton.classList.contains('selected-weapon')) userScissorButton.classList.toggle('selected-weapon');
+    let computerChoice = getComputerChoice();
+    chooseCorrectWeapon(computerChoice);
+    playRound("paper", computerChoice);
+    scoreChecker(userLives, computerLives);
+});
+
+userScissorButton.addEventListener('click', () => {
+    userScissorButton.classList.toggle('selected-weapon');
+    if (userPaperButton.classList.contains('selected-weapon')) userPaperButton.classList.toggle('selected-weapon');
+    if (userRockButton.classList.contains('selected-weapon')) userRockButton.classList.toggle('selected-weapon');
+    let computerChoice = getComputerChoice();
+    chooseCorrectWeapon(computerChoice);
+    playRound("scissor", computerChoice);
+    scoreChecker(userLives, computerLives);
+});
